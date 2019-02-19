8bd76dd51f277d90c2d0e9eba7d4db21dd220510
杜金银 jinyin.du <jinyin.du@weimob.com>
2019-02-19 22:43:10 +0800

    master

--- packages/swan-aa/index.js
+++ packages/swan-aa/index.js
@@ -7,14 +7,6 @@ const login = () =>{
 const login1 = () =>{
   console.log('login1')
 }
-const login2 = () =>{
-  console.log('login2')
-}
-const login3 = () =>{
-  console.log('login2')
-}
 
 login()
 login1()
-login2()
-login3()
